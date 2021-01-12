3a3c9a5871bd0049482a85c5b2e738ae0b7fcd4b
ToraMullings316 <tora.mullings@stonybrook.edu>
2021-01-11 22:25:57 -0500

    Add Grid for recipe layout

--- client/src/components/pages/EditRecipePage.jsx
+++ client/src/components/pages/EditRecipePage.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, StepContent, Stepper, Step, StepLabel } from '@material-ui/core';
+import { Box, Grid, Paper, StepContent, Stepper, Step, StepLabel, TextField, Typography } from '@material-ui/core';
 import UserContext from '../../contexts/UserContext';
 
 // Slate
@@ -11,7 +11,21 @@ import { createEditor } from 'slate'
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from 'slate-react'
 
+const useStyles = makeStyles((theme) => ({
+    parentBox: {
+        marginTop: '5%',
+        marginLeft: '10%',
+        marginRight: '10%',
+    },
+    sectionHeadings: {
+        variant: 'h2',
+        align: 'left'
+    }
+  
+  }));
+
 export default function EditRecipePage(props) {
+    const classes = useStyles();
     const history = useHistory();
     const { user } = useContext(UserContext);
     const [recipe, setRecipe] = useState(null);
@@ -30,17 +44,34 @@ export default function EditRecipePage(props) {
 
     return(
         <Box
-            style={{
-                marginTop: '10%',
-            }}
+            className={classes.parentBox}
         >
-            <Slate
-                editor={editor}
-                value={value}
-                onChange={newValue => setValue(newValue)}
-            >
-                <Editable />
-            </Slate>
+            <Grid container spacing={10} direction="column">
+                <Grid container item spacing={3} direction="column">
+                    <Grid container item xs={4} spacing={3}>
+                        <Typography variant="h3" align="left">Title</Typography>
+                    </Grid>
+                    <Grid container item xs={4} spacing={3}>
+                        <TextField variant="outlined" color="black" align="left" />
+                    </Grid>
+                </Grid>
+                <Grid container item spacing={3} direction="column">
+                    <Grid container item xs={4} spacing={3}>
+                        <Typography variant="h3" align="left">Recipe</Typography>
+                    </Grid>
+                    <Grid container item xs={4} spacing={3}>
+                        <Paper elevation={3}> Sameday
+                            <Slate
+                                editor={editor}
+                                value={value}
+                                onChange={newValue => setValue(newValue)}
+                            >
+                                <Editable />
+                            </Slate>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Grid>
         </Box>
     );
 }
